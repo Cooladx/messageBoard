ca405e6efd589b89c02b00a64737c4874eddb69a
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-11-15 12:18:06 -0500

    feat: add views and objects to be sent to index.ejs with a defined route.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,59 @@
 import express from "express";
-import 'dotenv/config'
-
+import "dotenv/config";
+import path from "node:path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
 
-const PORT: number = Number(process.env.PORT) || 3000;
+// Because dirname is through cjs, this is a fix to import dirname directly
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "../public");
+app.use(express.static(assetsPath));
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Good top of the morning to you!",
+    user: "Villa",
+    added: new Date(),
+  },
+  {
+    text: "Wsp people!",
+    user: "Linda",
+    added: new Date(),
+  },
+  {
+    text: "friends of the world!",
+    user: "Mike",
+    added: new Date(),
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { messages: messages });
+});
+
+const PORT: number = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
