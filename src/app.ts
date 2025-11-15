d54622285c66276aff7fbf4d906411b2d5fd5763
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-11-15 17:16:30 -0500

    refactor: fix seperation of concerns by adding messageRouter to take care of the task of POST/GET messages

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,13 @@ import path from "node:path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-import {messageRouter} from "./routes/index.js";
+import {indexRouter} from "./routes/indexRouter.js";
+import { messageRouter } from "./routes/messageRouter.js";
 
 const app = express();
+
+// allows the parse for the form data into req.body
+app.use(express.urlencoded({ extended: true }));
 
 // Because dirname is through cjs, this is a fix to import dirname directly
 const __filename = fileURLToPath(import.meta.url);
@@ -18,10 +22,13 @@ app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "../public");
 app.use(express.static(assetsPath));
 
+// Main page for indexRouter
+app.use("/", indexRouter)
 
-app.use("/", messageRouter)
+// Pages for sending message and viewing specific id message
+app.use("/new", messageRouter)
 
-const PORT: number = Number(process.env.PORT) || 3000;
+export const PORT: number = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
