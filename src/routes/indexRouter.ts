a98681e7c4b16f5e15b2539b7085b6ad7e9e7ee1
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-11-15 17:19:28 -0500

    refactor: Removed messageRouter logic and migrated to other file. Also added indexing of IDs to unique messages

---
+++ src/routes/indexRouter.ts
@@ -0,0 +1,40 @@
+import express, { Router } from "express";
+import { PORT } from "../app.js";
+export const indexRouter: Router = express.Router();
+
+
+
+type message = {
+  id: number,
+  text: string,
+  user: string,
+  added: Date
+}
+
+export const messages: message[] = [
+ 
+  {
+    id: 1,
+    text: "Good top of the morning to you!",
+    user: "Villa",
+    added: new Date(),
+  },
+  {
+     id: 2,
+    text: "Wsp people!",
+    user: "Linda",
+    added: new Date(),
+  },
+  {
+    id: 3,
+    text: "friends of the world!",
+    user: "Mike",
+    added: new Date(),
+  },
+];
+
+indexRouter.get("/", (req, res) => {
+    res.render("index", {  title: "Mini Messageboard", messages: messages, PORT: PORT });
+})
+
+
