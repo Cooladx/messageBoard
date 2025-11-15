d2698e48b44e93757b732863f45dc7221b3da412
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-11-15 13:59:47 -0500

    refactor: using routes now to lower tech debt and keep maintability high through esm modules

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import path from "node:path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
+import {messageRouter} from "./routes/index.js";
+
 const app = express();
 
 // Because dirname is through cjs, this is a fix to import dirname directly
@@ -16,42 +18,8 @@ app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "../public");
 app.use(express.static(assetsPath));
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Good top of the morning to you!",
-    user: "Villa",
-    added: new Date(),
-  },
-  {
-    text: "Wsp people!",
-    user: "Linda",
-    added: new Date(),
-  },
-  {
-    text: "friends of the world!",
-    user: "Mike",
-    added: new Date(),
-  },
-];
 
-app.get("/", (req, res) => {
-  res.render("index", { messages: messages });
-});
+app.use("/", messageRouter)
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
