3c5b6ac153869dbd1540ee995ec720a3d659aa12
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-11-21 15:05:44 -0500

    index router finished for now as for migrating to mvc logic and routes are done for get requests

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -1,37 +1,8 @@
 // src/routes/indexRouter.ts
 
 import express, { Router } from "express";
-import { PORT } from "../app.js";
-
-import { getAllMessages } from "../controllers/userController.js";
+import { getMessages } from "../controllers/userController.js";
 export const indexRouter: Router = express.Router();
 
-type message = {
-  id: number;
-  text: string;
-  user: string;
-  added: Date;
-};
 
-export const messages: message[] = [
-  {
-    id: 1,
-    text: "Good top of the morning to you!",
-    user: "Villa",
-    added: new Date(),
-  },
-  {
-    id: 2,
-    text: "Wsp people!",
-    user: "Linda",
-    added: new Date(),
-  },
-  {
-    id: 3,
-    text: "friends of the world!",
-    user: "Mike",
-    added: new Date(),
-  },
-];
-
-indexRouter.get("/", getAllMessages);
+indexRouter.get("/", getMessages);
