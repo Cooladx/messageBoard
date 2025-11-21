610c5158da3f42f305712db1879e5a2933a30f54
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-11-20 23:00:59 -0500

    controller to handle the logic for the routes regarding user forms and messages.

---
+++ src/controllers/userController.ts
@@ -0,0 +1,44 @@
+// src/controllers/userController.ts
+
+import type { Request, Response } from "express";
+import { messages } from "../routes/indexRouter.js";
+import { PORT } from "../app.js";
+
+
+const getAllMessages = async (req: Request, res: Response) => {
+    res.render("index", {
+    title: "Mini Messageboard",
+    messages: messages,
+    PORT: PORT,
+  });
+}
+
+
+const getUserForm = async (req: Request, res: Response) => {
+    res.render("form");
+}
+
+const createUsernamePost = async (req: Request, res: Response) => {
+    const {messageUser, messageText} = req.body;
+
+  // Get id from the last element in the array, otherwise set it to 0 because array is empty
+  let lastId: number = messages.at(-1)?.id ?? 0;
+
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+    id: lastId + 1,
+  });
+  res.redirect("/");
+  console.log(messages);
+}
+
+const getUserId = async (req: Request, res: Response) => {
+  let routeId = messages.find(
+    (message) => message.id === Number(req.params.id),
+  );
+  res.render("message", { routeId: routeId, PORT: PORT });
+}
+
+export {getUserForm, createUsernamePost, getUserId, getAllMessages};
